983e3993214e9ab797332b10d804c6f1fd5a1f19
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-16 13:30:06 -0700

    Implement some special keys that send canned messages to the server, for everyone to see.

--- input.js
+++ input.js
@@ -31,19 +31,44 @@ const handleUserInput = function (key) {
     case `\u001b[B`: //DOWN arrow
     case "S":
     case "s":
-          connection.write("Move: down");
+      connection.write("Move: down");
       break;
     // Send the RIGHT command to the server
     case `\u001b[C`: //RIGHT arrow
     case "D":
     case "d":
-        connection.write("Move: right");
+      connection.write("Move: right");
       break;
     // Send the LEFT command to the server
     case `\u001b[D`: //LEFT arrow
     case "A": 
     case "a":
       connection.write("Move: left");
+      break;
+    // Possible 'emotes'
+    case "O":
+    case "o":
+      connection.write("Say: Ouch!");
+      break;
+    case "H":
+    case "h":
+      connection.write("Say: Hurray!");
+      break;
+    case "K":
+    case "k":
+      connection.write("Say: One less sneak...");
+      break;
+    case "L":
+    case "l":
+      connection.write("Say: LOL!!");
+      break;
+    case "G":
+    case "g":
+      connection.write("Say: Good game");
+      break;
+    case "M":
+    case "m":
+      connection.write("Say: Grrrrrrr..!");
       break;
   }
 };
