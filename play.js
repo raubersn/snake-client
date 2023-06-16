ce0c45c0f33df596af88bea35f4c88797abb196e
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-16 09:29:54 -0700

    Create a function and register it as the "data" callback handler for stdin. Ensure we can terminate the game using ctrl+c

--- play.js
+++ play.js
@@ -2,4 +2,28 @@
 const {connect} = require("./client.js");
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
+
+// setup interface to handle user input from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", key => handleUserInput(key));
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  switch(key) {
+    // If the user wishes to quit (CTRL+C), terminate game execution  
+    case `\u0003`:
+      console.log("Your wish is my command (QUITTER!)");
+      process.exit();
+  }
+};
+
+setupInput();
+
