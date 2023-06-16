c61218cd71d0b224a32149b18de09a9ff4e296aa
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-16 08:37:28 -0700

    Print a message to the screen when the connection is successfully established and send the string "Name: ___" to the server, upon connection

--- client.js
+++ client.js
@@ -3,17 +3,26 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "127.0.0.1", // IP address
+    host: "localhost", // IP address
     port: 50541// PORT number
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // provide feedback to the client as soon as the connection is stablihed
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server.");
+    conn.write("Name: RSN");
+  });
+
   // handle incoming data and console.log it for the player
   conn.on("data", x => console.log(x));
+
+  
 
   return conn;
 };
 
+//Exporting as an object to be used in other modules
 module.exports = {connect};
--- play.js
+++ play.js
@@ -1,3 +1,4 @@
+//Importing the CP connection from the client module
 const {connect} = require("./client.js");
 
 console.log("Connecting ...");
